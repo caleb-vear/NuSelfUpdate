77f8c0e506d316929bbd2aad13be1b648e2a223c
Caleb Vear <caleb.vear@gmail.com>
2011-09-10 13:56:31 +1000

    Cleaned up some left over cruft from the last refactor
    --HG--
    rename : src/Tests/Helpers/AppUpdaters.cs => src/Tests/Helpers/TestUpdaterConfig.cs

---
+++ src/Tests/Helpers/TestUpdaterConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NSubstitute;
+using NuGet;
+
+namespace NuSelfUpdate.Tests.Helpers
+{
+    public class TestUpdaterConfig : AppUpdaterConfig
+    {
+        public IEnumerable<IPackage> PublishedPackages {set { PackageRepositoryFactory = PackageRepositoryFactories.Create(value); }}
+
+        public TestUpdaterConfig(Version installedVersion)
+        {
+            PackageSource = "repository";
+            AppPackageId = "package";
+            PackageRepositoryFactory = PackageRepositoryFactories.Create(Enumerable.Empty<IPackage>());
+            AppVersionProvider = Substitute.For<IAppVersionProvider>();
+            AppVersionProvider.CurrentVersion.Returns(installedVersion);
+            UpdatePrepDirectoryStrategy = new TestPrepDirectoryStrategy();
+            FileSystem = new MockFileSystem();
+        }
+    }
+}
