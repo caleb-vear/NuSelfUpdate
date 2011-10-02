602dbfc693ab2da8b88a0753283e3fd993917c34
Caleb Vear <caleb.vear@gmail.com>
2011-10-02 14:50:53 +1100

    Added Version.cs which will be used to control the versioning

--- src/NuSelfUpdate/Properties/AssemblyInfo.cs
+++ src/NuSelfUpdate/Properties/AssemblyInfo.cs
@@ -4,14 +4,11 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NuSelfUpdate")]
 [assembly: AssemblyDescription("Automatically update your applications via NuGet.")]
-[assembly: AssemblyCompany("Caleb Vear")]
+[assembly: AssemblyCompany("NuSelfUpdate Contributors")]
 [assembly: AssemblyProduct("NuSelfUpdate")]
-[assembly: AssemblyCopyright("Copyright © Caleb Vear 2011")]
+[assembly: AssemblyCopyright("Copyright © 2011 NuSelfUpdate Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("141a8690-1533-4166-9167-140782da8762")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/Tests/Properties/AssemblyInfo.cs
+++ src/Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NuSelfUpdate Tests")]
 [assembly: AssemblyDescription("Tests for NuSelfUpdate")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Caleb Vear")]
+[assembly: AssemblyCompany("NuSelfUpdate Contributors")]
 [assembly: AssemblyProduct("NuSelfUpdate.Tests")]
-[assembly: AssemblyCopyright("Copyright © Caleb Vear 2011")]
+[assembly: AssemblyCopyright("Copyright © 2011 NuSelfUpdate Contributors")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("614d069d-d4db-4bb9-a21c-199e40b83e9c")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
